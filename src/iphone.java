8eaf02dc4164766068601b17794a3619c488b942
JoaoRibeironetogit config --global user.email joaoribeirocoelhoneto@gmail.comgit config user.namegit config user.emailgit config user.namegit config init.defaultbranchgit config --global user.name JoaoRibeironeto <joaoribeirocoelhoneto@gmail.com>
2023-10-22 15:52:27 -0300

    iphone

--- src/iphone.java
+++ src/iphone.java
@@ -1,103 +1,26 @@
-public class Desafio {
+package model;
 
-  public static void main(String[] args) {
-    // Lê o tipo de cofre (primeira linha da entrada)
-    Scanner scanner = new Scanner(System.in);
-    String tipoCofre = scanner.nextLine();
-
-    // Cria o cofre
-    Cofre cofre;
-    if (tipoCofre.equalsIgnoreCase("digital")) {
-      cofre = criarCofre(scanner);
-    } else if (tipoCofre.equalsIgnoreCase("fisico")) {
-      cofre = criarCofre();
-    } else {
-      System.out.println("Tipo de cofre inválido!");
-      return;
-    }
-
-    // Verifica se o cofre está aberto
-    if (cofre.verificarAbertura()) {
-      // O cofre está aberto
-      cofre.imprimirInformacoes();
-    } else {
-      // O cofre está fechado
-      System.out.println("Cofre fechado!");
-    }
-  }
-
-  private static Cofre criarCofre(Scanner scanner) {
-    return new CofreDigital(scanner.nextInt());
-  }
-
-  private static Cofre criarCofre() {
-    return new CofreFisico();
-  }
+public interface ReprodutorMusical {
+  void play();
+  void pause();
+  void select();
 }
 
-class Cofre {
-
-  protected String tipo;
-  protected String metodoAbertura;
-
-  public Cofre(String tipo, String metodoAbertura) {
-    this.tipo = tipo;
-    this.metodoAbertura = metodoAbertura;
-  }
-
-  public boolean verificarAbertura() {
-    // Solicita a senha do usuário
-    System.out.println("Digite a senha do cofre digital:");
-    String senhaUsuario = scanner.next();
-
-    // Converte a senha do usuário para um inteiro
-    int senhaUsuarioInt = Integer.parseInt(senhaUsuario);
-
-    // Verifica se a senha do usuário é igual à senha do cofre
-    return senhaUsuarioInt == this.senha;
-  }
-
-  public void imprimirInformacoes() {
-    System.out.println("Tipo: " + this.tipo);
-    System.out.println("Metodo de abertura: " + this.metodoAbertura);
-  }
+public interface AparelhoTelefônico {
+  void ligar();
+  void atender();
 }
 
-class CofreDigital extends Cofre {
-
-  private int senha;
-
-  public CofreDigital(int senha) {
-    super("Cofre Digital", "Senha");
-    this.senha = senha;
-  }
-
-  // Solicita a senha do usuário
-  System.out.println("Digite a senha do cofre digital:");
-  String senhaUsuario = scanner.next();
-
-  // Converte a senha do usuário para um inteiro
-  int senhaUsuarioInt = Integer.parseInt(senhaUsuario);
-
-  // Verifica se a senha do usuário é igual à senha do cofre
-  return senhaUsuarioInt == this.senha;
-
-  // Imprime as informações do cofre
-  System.out.println("Tipo: " + this.tipo);
-  System.out.println("Metodo de abertura: " + this.metodoAbertura);
-  System.out.println("Senha: " + this.senha);
+public interface NavegadorInternet {
+  void exibirPágina();
+  void adicionarNovaAba();
+ void atualizarPágina();
 }
 
-class CofreFisico extends Cofre {
 
-  public CofreFisico() {
-    super("Cofre Fisico", "Chave");
+
+  
+
+ 
   }
-
-  // Por padrão, um cofre físico é considerado aberto
-  return true;
-
-  // Imprime as informações do cofre
-  System.out.println("Tipo: " + this.tipo);
-  System.out.println("Metodo de abertura: " + this.metodoAbertura);
 }
